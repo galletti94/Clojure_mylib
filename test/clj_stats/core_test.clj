598d1d19de7fe946d8d5a08a4e2cd4013049ed8c
galletti <galletti@bu.edu>
2017-11-06 14:45:28 -0500

    Updating: adding some tests

--- test/clj_stats/core_test.clj
+++ test/clj_stats/core_test.clj
@@ -1,21 +1,30 @@
-(ns clj-stats.core-test
+(ns clj_stats.core_test
   (:require [clojure.test :refer :all]
-            [clj-stats.core :refer :all]))
+            [clj_stats.core :refer :all]))
+
+(def test_var '(1 2 3 4 5 6 7 8 9 10))
 
 (deftest foldleft
   	 (testing "foldleft failed."
-    	 (is (= (list_foldleft 0 '(1 2 3 4) (fn [x y] (+ x y)) ) 10))
+    	 (is (= (list_foldleft 0 test_var +) 55))
     	 ))
 
-(deftest foldright
-	 (testing "foldright failed."
-	 (is (= (list_foldright 0 '(1 2 3 4) (fn [x y] (- x y)) ) -2))
-	 ))
-
+(deftest mean
+	 (testing "list_mean failed."
+	 (is (= (list_mean test_var) 11/2)
+	 )))
 
 (deftest length
-	 (testing "length failed."
-	 (is (= (list_length '(1 2 3 4 5 6 7 8 9)) 9))
+	 (testing "length failed"
+	 (is (= (list_length test_var) (nth (lazy_rolling_length test_var) 9)))
 	 ))
 
+(deftest test_streamize
+	 (testing "streamize failed."
+	 (is (and (= (first (streamize test_var)) (first test_var)) (=(nth (streamize test_var) 5) (nth test_var 5)))
+	 )))
 
+(deftest lazymean
+	 (testing "lasy_leftroll failed"
+	 (is (= (nth (lazy_rolling_mean test_var) (- (list_length test_var) 1)) (list_mean test_var))
+	 )))
